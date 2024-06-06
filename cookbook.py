2621e2ef9639d58643f9fc8e6e0a2502323101f8
Sergey Tormozov <web@stormozov.ru>
2024-06-06 19:53:31 +0300

    [feat] Generate shopping list
    Added function to generate shopping list based on dishes and person count
    
    Refs: #2

--- cookbook.py
+++ cookbook.py
@@ -62,4 +62,52 @@ def process_recipes(source_file: str, coding_standard: str) -> dict:
 		return cook_book
 
 
-pprint.pprint(process_recipes(recipes_file_path, 'UTF-8'))
+# pprint.pprint(process_recipes(recipes_file_path, 'UTF-8'))
+
+cookbook: dict[str, list[dict]] = process_recipes(recipes_file_path, 'UTF-8')
+
+
+def get_shop_list_by_dishes(source: dict, dishes: list[str], person_count: int) -> dict[str, dict[str, int]]:
+	"""
+	Generate a shopping list based on the provided dishes, number of people, and ingredients.
+
+	Parameters:
+		source (dict): A dictionary where the keys are dish names and the values are lists
+		of dictionaries representing ingredients.
+		dishes (list[str]): A list of dish names to include in the shopping list.
+		person_count (int): The number of people to consider when calculating
+		ingredient quantities.
+
+	Returns:
+		dict[str, dict[str, int]]: A dictionary where the keys are ingredient names and the
+		values are dictionaries containing the total quantity needed (based on person count) and the
+		measure of the ingredient.
+	"""
+	generated_shop_list: dict = {}
+
+	for dish in dishes:
+		for ingredient in source[dish]:
+			# Multiply the quantity of the ingredient by the number of people
+			ingredient_name = ingredient['ingredient_name']
+			# Create a dictionary with the quantity and measure
+			ingredient_dict = {
+				'quantity': ingredient['quantity'] * person_count,
+				'measure': ingredient['measure']
+			}
+
+			# Check if the ingredient is already in the generated shop list
+			if ingredient_name in generated_shop_list:
+				# If the ingredient is already in the generated shop list, we add the quantity
+				generated_shop_list[ingredient_name]['quantity'] += ingredient_dict['quantity']
+			else:
+				# If the ingredient is not in the generated shop list, we add it
+				generated_shop_list[ingredient_name] = {
+					'quantity': ingredient_dict['quantity'],
+					'measure': ingredient_dict['measure']
+				}
+
+	return generated_shop_list
+
+
+shop_list = get_shop_list_by_dishes(cookbook, ['Запеченный картофель', 'Омлет'], 2)
+pprint.pprint(shop_list)
