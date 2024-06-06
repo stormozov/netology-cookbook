aa5803079f8b0894c7a8c8c7f88ba4fbbd9f5113
Sergey Tormozov <web@stormozov.ru>
2024-06-06 14:27:11 +0300

    [refactor] Added "process_recipes" func
    Now, the processing of recipes from files takes place inside the process_recipes function.
    
    Refs: #1

--- cookbook.py
+++ cookbook.py
@@ -1,17 +1,17 @@
-# Импортирую модулю OS для работы с операционной системой
+# Importing the OS module to work with the operating system
+# https://docs.python.org/3/library/os.html
 import os
-# Импортирую модулю Pprint для лучшего вывода объемной информации в консоль для проверки
+# I import the Pprint module to better output bulk information to the console for verification
+# https://docs.python.org/3/library/pprint.html
 import pprint
 
-# Получаю абсолютный путь к файлу с рецептами
+# I get the absolute path to the recipe file
 recipes_file_path = os.path.join(os.getcwd(), 'source_files', 'recipes.txt')
-# Создаю пустой словарь, в котором будут храниться данные из нужного файла
-cook_book: dict = {}
 
 
-def process_dish(source):
+def process_ing_dish(source):
 	"""
-	Process the dish information from the provided file.
+	Process the ingredients of the dish from the provided file.
 
 	Parameters:
 		source (file object): The file object containing the dish information.
@@ -26,6 +26,7 @@ def process_dish(source):
 	for _ in range(num_ingredients):
 		line: str = source.readline().strip()
 		if not line:
+			# If the line is empty, we raise an exception
 			raise ValueError("Empty line encountered")
 		ingredient_name, quantity, measure = line.split(' | ')
 		dish_ingredients.append({
@@ -37,11 +38,28 @@ def process_dish(source):
 	return dish_ingredients
 
 
-with open(recipes_file_path, encoding='UTF-8') as file:
-	for line in file:
-		dish_name: str = line.strip()
-		while dish_name:
-			cook_book[dish_name] = process_dish(file)
-			dish_name = file.readline().strip()
+def process_recipes(source_file: str, coding_standard: str) -> dict:
+	"""
+	Process the recipes from the provided file.
 
-pprint.pprint(cook_book)
+	Parameters:
+		source_file (str): The path to the file containing the recipes.
+		coding_standard (str): The coding standard of the source file.
+	Returns:
+		dict: A dictionary where the keys are the dish names and the values are lists
+		of dictionaries, where each dictionary represents an ingredient of the dish.
+		Each dictionary contains the keys 'ingredient_name', 'quantity' and 'measure'.
+	"""
+	with open(source_file, encoding=coding_standard) as file:
+		# I create an empty dictionary to store data from the required file
+		cook_book: dict[str, list[dict]] = {}
+		for line in file:
+			# We go through each line of the recipe file and
+			# call the process_dish function to process the ingredients
+			dish_name: str = line.strip()
+			cook_book[dish_name] = process_ing_dish(file)
+			dish_name: str = file.readline().strip()
+		return cook_book
+
+
+pprint.pprint(process_recipes(recipes_file_path, 'UTF-8'))
