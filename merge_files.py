109bb7e85e98e16b378d718df5ae64d1bc5f7115
Sergey Tormozov <web@stormozov.ru>
2024-06-08 15:34:12 +0300

    [refactor] A function to get a file path based on its extension
    I have written a new function that takes a file extension as an input and returns the complete path to the corresponding file. This function searches for the required files in all directories and subdirectories within the root folder of the project.
    
    Refs: #3

--- merge_files.py
+++ merge_files.py
@@ -4,14 +4,34 @@ import pprint
 UNICODE = 'UTF-8'
 
 
-def generate_file_path(file_name, folder_name='source_files'):
-	return os.path.join(os.getcwd(), folder_name, file_name)
+def get_files_by_extension(file_extension: str) -> dict:
+	"""
+	Recursively traverses a given directory path, collecting and returning a sorted dictionary of
+	all files with their full path within it.
+
+	Args:
+	- file_extension: The file extension to filter the files by.
+
+	Returns:
+		- A dictionary where keys are file names and values are their full paths found within
+	the specified directory path.
+	"""
+	directory_path: str = os.path.join(os.path.dirname(__file__))
+
+	if not os.path.exists(directory_path):
+		raise ValueError(f'Directory "{directory_path}" does not exist')
+
+	file_dict: dict = {}
+
+	for root, dirs, files in os.walk(directory_path):
+		for file in files:
+			if file_extension is None or file.endswith(file_extension):
+				file_dict[file] = os.path.join(root, file)
+
+	return file_dict
 
 
-file_1 = generate_file_path('1.txt', 'source_files/sorted')
-file_2 = generate_file_path('2.txt', 'source_files/sorted')
-file_3 = generate_file_path('3.txt', 'source_files/sorted')
-result_file = generate_file_path('merged_files.txt')
+all_txt_file_paths: dict = get_files_by_extension('.txt')
 
 
 def count_number_rows(source):
@@ -45,4 +65,5 @@ def merge_files(source_file_1, source_file_2, source_file_3, result_file):
 						result.write(f'{i3}')
 
 
-merge_files(file_1, file_2, file_3, result_file)
+merge_files(all_txt_file_paths['1.txt'], all_txt_file_paths['2.txt'], all_txt_file_paths['3.txt'],
+			all_txt_file_paths['merged_files.txt'])
