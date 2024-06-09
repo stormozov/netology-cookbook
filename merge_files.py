31fc7f4d9b68660b1a2bde3fe4853a61963b2bf9
Sergey Tormozov <web@stormozov.ru>
2024-06-09 21:13:39 +0300

    [refactor] Added error handling in merge_files.py
    Refs: #5

--- merge_files.py
+++ merge_files.py
@@ -7,27 +7,30 @@ all_txt_file_paths: dict = gfp.get_files_by_extension('source_files', '.txt')
 
 
 def merge_files(source_files: list, result_file: str):
-	temp_list = []
-	for source_file in source_files:
-		with open(source_file, 'r', encoding=UNICODE) as f:
-			file_name = os.path.basename(source_file)
-			file_content = f.readlines()
-			temp_list.append({
-				file_name: {
-					'file_name': file_name,
-					'count_lines': len(file_content or 0),
-					'content': file_content
-				}
-			})
+    temp_list = []
+    for source_file in source_files:
+        try:
+            with open(source_file, 'r', encoding='utf-8') as f:
+                file_name = os.path.basename(source_file)
+                file_content = f.readlines()
+                temp_list.append({
+                    file_name: {
+                        'file_name': file_name,
+                        'count_lines': len(file_content),
+                        'content': file_content
+                    }
+                })
+        except (FileNotFoundError, OSError) as e:
+            print(f"An error occurred while reading {source_file}: {e}")
 
-	temp_list.sort(key=lambda x: x[list(x.keys())[0]]['count_lines'])
+    temp_list.sort(key=lambda x: x[list(x.keys())[0]]['count_lines'])
 
-	with open(result_file, 'w', encoding=UNICODE) as result:
-		for dictionary in temp_list:
-			for info in dictionary:
-				result.write(f'{info}\n{dictionary[info]["count_lines"]}\n')
-				for i3 in dictionary[info]['content']:
-					result.write(f'{i3}')
+    with open(result_file, 'w', encoding='utf-8') as result:
+        for dictionary in temp_list:
+            for info in dictionary:
+                result.write(f'{info}\n{dictionary[info]["count_lines"]}\n')
+                for i3 in dictionary[info]['content']:
+                    result.write(f'{i3}')
 
 
 merge_files(
